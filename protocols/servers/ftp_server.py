e7bdef31d0e93fc3da6e09c9fc0201884b10dbf4
Mark Hunsinger <mark.j.hunsinger@gmail.com>
2026-06-16 16:24:26 -0400

    Auto-detect public IP for FTP PASV masquerade address

--- protocols/servers/ftp_server.py
+++ protocols/servers/ftp_server.py
@@ -1,9 +1,22 @@
 import os
 import socket
 import sys
+import urllib.request
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
+
+
+def _detect_public_ip():
+    # EC2 instance metadata (fast, no internet hop)
+    for url in ('http://169.254.169.254/latest/meta-data/public-ipv4',
+                'https://api.ipify.org',
+                'https://checkip.amazonaws.com'):
+        try:
+            return urllib.request.urlopen(url, timeout=2).read().decode().strip()
+        except Exception:
+            continue
+    return None
 
 
 class Server:
@@ -43,7 +56,10 @@ class Server:
             # Define a customized banner (string returned when client connects)
             handler.banner = "Connecting to Egress-Assess's FTP server!"
             # Define public address and passive ports making NAT configurations more predictable
-            handler.masquerade_address = self.ip
+            masq = self.ip or _detect_public_ip()
+            if masq:
+                print(f'[*] FTP masquerade address: {masq}')
+            handler.masquerade_address = masq
             handler.passive_ports = list(range(60000, 60100))
 
             try:
